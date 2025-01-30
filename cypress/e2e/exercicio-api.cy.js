5d66565f2f2eaba4c6fab7aeb20533e46256984b
joao pedro <joaomhass4@gmail.com>
2025-01-29 22:11:33 -0300

    primeiros testes da tarefa

--- cypress/e2e/exercicio-api.cy.js
+++ cypress/e2e/exercicio-api.cy.js
@@ -3,15 +3,34 @@
 describe('Testes da Funcionalidade Usuários', () => {
 
   it('Deve validar contrato de usuários', () => {
-    //TODO: 
+    //TODO:
   });
 
   it('Deve listar usuários cadastrados', () => {
-    //TODO: 
+    cy.request({
+      method: 'GET',
+      url: 'usuarios'
+    }).should((response) =>{
+      expect(response.status).equal(200)
+      expect(response.body).to.have.property('usuarios')
+      
+    })
+
   });
 
-  it('Deve cadastrar um usuário com sucesso', () => {
-    //TODO: 
+  it.only('Deve cadastrar um usuário com sucesso', () => {
+    cy.request({
+      method: 'POST',
+      url: 'usuarios',
+      body: {
+        "nome": "Pamela do Amaral",
+        "email": "pamela3@teste.com.br",
+        "password": "teste",
+        "administrador": "true"
+      }
+    }).should((response) =>{
+      expect(response.status).equal(201)
+    })
   });
 
   it('Deve validar um usuário com email inválido', () => {
